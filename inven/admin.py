3486b1c0cce5feafe69020df2f7c5fb3e7dbbb37
sieun <sieun960513@naver.com>
2022-01-17 15:55:26 +0900

    model 간단히 생성

--- inven/admin.py
+++ inven/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from inven.models import Tools, Users
+
+admin.site.register(Tools)
+admin.site.register(Users)
--- inven/models.py
+++ inven/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+class Users(models.Model):  # 사용자
+    name = models.CharField(max_length=200)
+    department = models.CharField(max_length=200)
+    position = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Tools(models.Model):  # 장비
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    tool = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.tool
+
+
